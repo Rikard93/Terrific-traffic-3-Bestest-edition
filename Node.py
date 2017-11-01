3cccfaebbc0bf3b50b5e13796e352b2fd45ef5ef
terrifictraffic <32098993+terrifictraffic@users.noreply.github.com>
2017-11-01 23:39:12 +0100

    Update Node.py

--- Node.py
+++ Node.py
@@ -3,3 +3,13 @@ class Node(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    #returnerar posisjonen til noden
+    def get_both_positions(self):
+        return self.x, self.y
+
+    def get_x_position(self):
+        return self.x
+
+    def get_y_position(self):
+        return self.y
